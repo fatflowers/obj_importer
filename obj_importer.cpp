ee2d608766dc150b738c972d0e18996c4c70b28c
sunlei99 <355890577@qq.com>
2014-09-26 23:16:05 +0800

    init obj_importer class

--- obj_importer.cpp
+++ obj_importer.cpp
@@ -1,5 +1,8 @@
 #include"obj_importer.h"
 
+/*
+ * init all the number member
+ * */
 obj_importer::obj_importer(string obj_filename): obj_filename(obj_filename){
 	fstream obj_file(obj_filename.c_str());
 	//assert(obj_file);
@@ -12,23 +15,27 @@ obj_importer::obj_importer(string obj_filename): obj_filename(obj_filename){
 	}
 	string line;
 	while(getline(obj_file, line)){
-		switch(line[0]){
-			case 'v':
-				vertice_number++;
-				break;
-			case 'f':
-				face_number++;
-				break;
-		}
+		// Vertice
+		if(line[0] == 'v' && line[1] == ' ')
+			vertice_number++;
+		// Face
+		else if(line[0] == 'f' && line[1] == ' ')
+			face_number++;Parameter space vertice
+		// Normal
+		else if(line[0] == 'v' && line[1] == 'n');
+		// Parameter space vertice
+		else if(line[0] == 'v' && line[1] == 'p');
 	}
-	
+	// 此处可能的改进：将文件流设为一个类成员，每次用完将其定位到特定位置
 	obj_file.close();
-	cout << vertice_number << endl << face_number << endl;
+	//cout << vertice_number << endl << face_number << endl;
+	
+	vertices.resize(vertice_number);
 }
 
 
 int main(){
-	obj_importer o(string("/home/simon/obj_importer/hirestoutatis.obj"));
+	obj_importer o(string("/home/simon/obj_importer/data.obj"));
 
 	return 0;
 }
